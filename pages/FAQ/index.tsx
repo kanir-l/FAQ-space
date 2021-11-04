3bf69587bb0c26d54caa081b8f242b5550cf0e4c
Patrick Edqvist <patrick.edqvist@gmail.com>
2021-11-04 10:12:35 +0100

    Update search bar and search result page

--- pages/FAQ/index.tsx
+++ pages/FAQ/index.tsx
@@ -1,13 +1,16 @@
 import type { NextPage } from 'next'
-import React, { ChangeEvent, useState } from 'react'
+import React, { ChangeEvent, FormEvent, useState } from 'react'
+import { useRouter } from 'next/router';
+
 // Components
 import Categories from 'components/FAQ/Categories/Categories'
 import SearchBar from 'components/Search/SearchBar'
+
 // Services
 import fetchGraphQL from 'services/contentful'
+
 // Interfaces
 import  { GetSubCategoryByGraphQL, ISubCategory } from 'interfaces/FAQ'
-
 
 interface PropsCategory {
   subCategories: ISubCategory[]
@@ -15,19 +18,36 @@ interface PropsCategory {
 
 const fag: NextPage<PropsCategory> = ( {subCategories} ) => {
   const [searchQuery, setSearchQuery] = useState("")
+  const router = useRouter();
 
   const onChange = (event: ChangeEvent<HTMLInputElement>) => {
     setSearchQuery(event.target.value)
   }
 
+  const handleOnSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    
+    // Remove any leading or trailing whitespaces from the search query
+    const withoutSpaces = searchQuery.trim();
+    
+    // Only send the user to the search result page if there is an actual search query
+    if (withoutSpaces.length > 0) {
+      router.push(`/faq/search/${withoutSpaces}`);
+    }
+  }
+
   return (
     <div>
       <div>
-        <SearchBar value={searchQuery} onChange={onChange}></SearchBar>
-        <Categories subCategories={subCategories}></Categories>
+        <SearchBar
+          value={searchQuery}
+          onChange={onChange}
+          onSubmit={handleOnSubmit}
+        />
+        <Categories subCategories={subCategories} />
       </div>
     </div>
-  )
+  );
 }
 export default fag
 
